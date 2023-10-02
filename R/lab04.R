0802ce06f94e3314cf84e5e2b94230b84f5563a8
Pierre Qi <qiqinyuan@gmail.com>
2023-10-02 23:04:19 +0200

    Add importFrom to functions,and adjust export in function header. Also add .DS_Store in .gitignore to avoid Mac temp file added to github

--- R/lab04.R
+++ R/lab04.R
@@ -17,6 +17,7 @@
 #' @param data Dataframe
 #' @param qr Boolean
 #' @return linreg object(list) contain all the calculated values regarding the linear regression
+#' @importFrom stats model.matrix pt
 #' @export 
 linreg <- function(formula, data, qr = FALSE) {
   
@@ -133,6 +134,12 @@ get_png <- function(filename) {
 #'
 #' Author: Koushik Pilla
 #' @param obj linreg object
+#' 
+#' @importFrom ggplot2 ggplot geom_path geom_point aes_string aes theme element_text labs theme_void annotation_custom
+#' @importFrom png readPNG
+#' @importFrom grid rasterGrob
+#' @importFrom gridExtra  grid.arrange
+#' 
 #' @export plot.linreg
 #' @export 
 
@@ -147,33 +154,21 @@ plot.linreg <- function(obj) {
                                       colour = "white"), 
       panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
                                       colour = "white"),
-      plot.title = element_text(color="blue2", face="bold", size="14"),
+      plot.title = element_text(color="blue2", face="bold", size="14",hjust = 0.5)
     )
   
   l <- get_png("LiU.png")
-  
-  # Create moving median here, and window size set to 5
-  window_size <- 5
 
   # setup data frame for 1st graph
   fitted_values <- as.data.frame(obj$fitted_values)
   residuals <- as.data.frame(obj$residuals)
   df_g1 <- data.frame(fitted_values,residuals)
-  length_moving_median <- length(residuals[,1]) - window_size + 1
-  
-  # Calculate the moving median values
-  residuals_moving_median <- sapply(1:length_moving_median, function(i) {
-    median(residuals[i:(i + window_size - 1),1])
-  })
-  
+ 
   # draw 1st graph
-  p1 <- ggplot(data = df_g1,mapping = aes_string(x = names(df_g1)[1], y = names(df_g1)[2])) +
+  p1 <- ggplot2::ggplot(data = df_g1,mapping = aes_string(x = names(df_g1)[1], y = names(df_g1)[2])) +
     geom_point(shape = 21) +
-    geom_line(data = data.frame(x = fitted_values[1:length_moving_median,1], y = residuals_moving_median), aes(x, y), color = "red", linetype = "dashed") +  # Add a red dashed moving median line
-    stat_smooth(method='lm', colour="blue", se=FALSE, span = 1,linetype = "dashed") +
+    stat_summary(fun="median",geom="line",linetype = "dashed",colour="red") +
     linkoping_theme +
-    
-    theme(plot.title = element_text(hjust = 0.5)) +
     labs(
       title = "Residuals vs Fitted",
       x = "Fitted values \n lm(Petal.Length ~ Species)",
@@ -187,19 +182,10 @@ plot.linreg <- function(obj) {
   residuals_new <- abs((residuals - miu) / sigma)
   df_g2 <- data.frame(fitted_values,residuals_new)
   
-  # calculate  moving median
-  length_new_median_value <- length(residuals_new[,1]) - window_size + 1
-  #Calculate the moving median values
-  residuals_new_moving_median <- sapply(1:length_new_median_value, function(i) {
-    median(residuals_new[i:(i + window_size - 1),1])
-  })
-  
   # draw 2nd graph
-  p2 <- ggplot(data = df_g2,mapping = aes_string(x = names(df_g2)[1], y = names(df_g2)[2])) +
+  p2 <- ggplot2::ggplot(data = df_g2,mapping = aes_string(x = names(df_g2)[1], y = names(df_g2)[2])) +
     geom_point(shape = 21) +
-    geom_line(data = data.frame(x = fitted_values[1:length_new_median_value,1], y = residuals_new_moving_median), aes(x, y), color = "red", linetype = "dashed") + 
-    stat_smooth(method='lm', colour="blue", se=FALSE, span = 1,linetype = "dashed") +
-    theme(plot.title = element_text(hjust = 0.5)) +
+    stat_summary(fun="median",geom="line",linetype = "dashed",colour="red") +
     linkoping_theme + 
     labs(
       title = "Scale Location",
@@ -211,8 +197,6 @@ plot.linreg <- function(obj) {
   p3 <- ggplot(mapping = aes(x = 0:1, y = 0.1)) +
       theme_void() +
       annotation_custom(l, xmin = .8, xmax = 1)
-  
-  # vertical layout (1 X 3) with customized height in percent
   grid.arrange(p1, p2, p3,heights = c(.46,.46,.08))
 }
 
@@ -244,6 +228,7 @@ resid.linreg <- function(obj){
 #' @param obj linreg object
 #' @export coef.linreg
 #' @export 
+
 coef.linreg <- function(obj){
   return(obj$coefficients)
 }
@@ -252,6 +237,7 @@ coef.linreg <- function(obj){
 #'
 #' Author: Koushik Pilla
 #' @param obj linreg object
+#' @importFrom methods show
 #' @export summary.linreg
 #' @export 
 summary.linreg <- function(obj){
